ec2265bc5f8d5820d86cec55c4baafb94c6b214c
Hassan El-Hajj <hhajj@mpiwg-berlin.mpg.de>
2022-10-13 18:06:43 +0200

    some docs and extra feature

--- practicalSPARQL.py
+++ practicalSPARQL.py
@@ -8,6 +8,7 @@ import numpy as np
 
 
 def path_valid(path:str)->bool:
+    """Takes a path and checks if it is valid"""
     if os.path.exists(path):
         return True
     else:
@@ -18,10 +19,18 @@ def path_valid(path:str)->bool:
 def stringify_SPARQL(
     query:str, 
     includesVariables = False, 
-    placeHolder = None, 
-    variables = None, 
+    variable_dict = None,
     is_file = False
     )->str:
+    """Function to modify a sparql query
+    Returns query string
+
+    query: str or path to .sparql file containing the query
+    includesVariable: bool, default False. True indicates that the SPARQL query contains a variable to be replaced
+    variable_dict: dict, default None. If passed, includes a pairwise dict of variable in the sparql and the values that should replace them. 
+    is_file: bool, default False. Flag, if true, query is a .sparql file to be opened and read. 
+
+    """
     
     # check if file is chosen instead of a variable query
     if is_file is True:
@@ -33,20 +42,23 @@ def stringify_SPARQL(
         with open(query, 'r') as f:
             query  = f.read()
     
+        
     if includesVariables is True:
-        for j in range(len(placeHolder)):
-            query = query.replace(str(placeHolder[j]), str(variables[j]))
+        for var in variable_dict:
+            query = query.replace(str(var), str(variable_dict[var]))
         return query
     else:
         return query
     
 
 class rdfGRAPH(rdflib.Graph):
+    """Subclass inheriting rdflib.Graph Class -> https://github.com/RDFLib/rdflib"""
+
     def __init__(self, store = "default", identifier = None, namespace_manager = None, base =  None):
         super().__init__(store, identifier, namespace_manager, base)
         
     def read_graph(self, graphPath:str):
-
+        """Parse a TTL graph"""
         if path_valid(graphPath) is False:
                 raise Exception('Query path invalid')
         # read graph 
@@ -65,14 +77,17 @@ class rdfGRAPH(rdflib.Graph):
         
 
 
- 
-    
-
 class practicalWrapper(SPARQLWrapper):
     def __init__(self, endpoint, updateEndpoint=None, returnFormat=..., defaultGraph=None, agent='sparqlwrapper 1.8.5 (rdflib.github.io/sparqlwrapper)'):
         super().__init__(endpoint, updateEndpoint, returnFormat, defaultGraph, agent)
     
     def select_as_dataframe(self, q:str):
+        """SELECT QUERY from a string variable
+        Returns pandas DF as CSV
+        
+        q: SPARQL query as string
+        
+        """
         import pandas as pd
         self.setQuery(q)
         if self.queryType != SELECT:
@@ -109,6 +124,10 @@ class practicalWrapper(SPARQLWrapper):
         return results
 
     def post_query(self, q:str):
+        """Post a SPARQL Query -> Insert or Delete.
+        q: SPARQL query as string
+        
+        """
         self.setQuery(q)
         if self.queryType != POST:
             raise ValueError('Only POST queries are accepted')
@@ -135,7 +154,14 @@ class practicalWrapper(SPARQLWrapper):
                 if counter == 4:
                     raise client.HTTPException('-- After several retires, operation ended --')            
 
-    def dump_as_ttl(self, q:str, outpath:str)->None:
+    def construct_as_ttl(self, q:str, outpath:str)->None:
+        """Construct a TTL file from a SPARQL query
+        returns a .ttl file
+
+        q: SPARQL query as str
+        outpath: output directory
+        
+        """
         # add exception for location and file place
         # add exception on query types
         with open(outpath, 'w') as mydump:
@@ -166,6 +192,33 @@ class practicalWrapper(SPARQLWrapper):
                         raise client.HTTPException('-- After several retires, operation ended --')            
             mydump.write(results.decode('utf-8'))
 
+    def dump_graph(self, graph:str, outpath:str):
+        """Dump a specific graph as TTL
+        graph: string of graph URI, if None, dump content of triple store
+        outpath: output directory
+        
+        """
+        if graph is None:
+            q = """
+            CONSTRUCT{
+                ?s ?p ?o
+                }
+            WHERE{
+                ?s ?p ?o
+                }
+            """
+            print('--- Processing Complete Graph --- This might take a while depending on the size of your database ---')
+            self.construct_as_ttl(q, outpath)
+        else:
+            q = """CONSTRUCT{
+                ?s ?p ?o
+                }
+            WHERE{GRAPH <{G}>{
+                ?s ?p ?o
+                    }
+                }
+            """.replace('{G}', graph)
+            self.construct_as_ttl(q, outpath)
 
         
 
