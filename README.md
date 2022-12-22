0f72d1f3424cf2b71bb40308e6f7e2732ac25d7a
Hassan El-Hajj <hhajj@mpiwg-berlin.mpg.de>
2022-12-22 16:07:54 +0100

    setup extras

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ The package is built on top of rdflib [https://rdflib.readthedocs.io/en/stable/i
 practicalSPARQL has two main classes, practicalWrapper which inherits from sparqlWrapper with a couple of extra functions to query some values directly to a dataframe, as well as some function to quickly construct TTL files. Error handeling is mostly done within these function to allow their quick usage with minimal code. <br>
 Check the [tutorial.ipynb](tutorial.ipynb) for some examples; which are executed again the Wikidata sparql endpoint. Some example queries are saved as .sparql files int he queries folder. 
 
+
+
+
--- setup.py
+++ setup.py
@@ -1,12 +1,27 @@
 from setuptools import setup
 
+
+with open('README.md', 'r') as f:
+    longDesc = f.read()
+
 setup(
     name="practicalSPARQL",
     version='0.0.1',
-    description="Small wrapper to simplify the interaction with SPARQL endpoints. Built on top of SPARQLwrapper and RDFlib",
+    description="Small wrapper to simplify the interaction with SPARQL endpoints. Built on top of sparqlwrapper and RDFlib",
     py_modules=["practicalSPARQL"],
-    package_dir={'':'src'}
-
-
-
+    package_dir={'':'src'},
+    author='hassanhajj910@gmail.com',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Operating Sysmte :: OS Independent",
+        "License :: MIT License" 
+    ],
+    long_description=longDesc,
+    long_description_content_type = "text/markdown",
+    install_requires = [
+        "SPARQLWrapper",
+        "rdflib",
+        "numpy"
+    ]
 )
